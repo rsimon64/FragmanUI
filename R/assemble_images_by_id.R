693b0961dd785ab5c7c972aa7138510083372245
Reinhard Simon <rsimon64@gmail.com>
2018-04-16 16:44:49 -0500

    working on reporting ...

---
+++ R/assemble_images_by_id.R
@@ -0,0 +1,4 @@
+assemble_images_by_id <- function(id) {
+  # assemble a markdown page with table of images channels(rows) by types(columns).
+  # first column: electro gel, 2nd column individual reading
+}
