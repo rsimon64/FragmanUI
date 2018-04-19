2bde350e94f59696049ee2a495923246ed333334
Reinhard Simon <rsimon64@gmail.com>
2018-04-19 12:00:36 -0500

    refactored error handling in #3

--- R/do_report_basic_addin.R
+++ R/do_report_basic_addin.R
@@ -1,21 +1,25 @@
 do_report_basic_addin <- function() {
   is.defined <- function(sym) class(try(sym, TRUE))!='try-error'
 
-  advice_folder <- function(){
+  advice_folder <- function(diagnosis){
+    message(diagnosis)
     message("Favor definir un directorio con datos: folder <- choose.dir()")
     rstudioapi::sendToConsole(paste0("folder <- choose.dir()"), execute = FALSE)
   }
 
-  if(is.defined(folder)) {
-    message("No existe el objeto 'folder' en la sesión de R.")
-    advice_folder()
+  if(is.defined("folder")) {
+    advice_folder("No existe el objeto 'folder' en la sesión de R.")
     return()
   }
 
+
   if(!dir.exists(folder)) {
-    message("No existe el objeto 'folder' indicado.")
-    advice_folder()
+    advice_folder("No existe el objeto 'folder' indicado.")
+    return()
   }
+
+
+  # TODO check on presence of correct file(s)
 
   FragmanUI:::do_report_basic(folder)
 
