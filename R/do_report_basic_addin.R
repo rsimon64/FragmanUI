6ed1efcf67ab11656ff263c66bfc0d9791101aaf
Reinhard Simon <rsimon64@gmail.com>
2018-04-19 11:56:58 -0500

    Added checks for report #3.

--- R/do_report_basic_addin.R
+++ R/do_report_basic_addin.R
@@ -1,5 +1,21 @@
 do_report_basic_addin <- function() {
+  is.defined <- function(sym) class(try(sym, TRUE))!='try-error'
 
+  advice_folder <- function(){
+    message("Favor definir un directorio con datos: folder <- choose.dir()")
+    rstudioapi::sendToConsole(paste0("folder <- choose.dir()"), execute = FALSE)
+  }
+
+  if(is.defined(folder)) {
+    message("No existe el objeto 'folder' en la sesión de R.")
+    advice_folder()
+    return()
+  }
+
+  if(!dir.exists(folder)) {
+    message("No existe el objeto 'folder' indicado.")
+    advice_folder()
+  }
 
   FragmanUI:::do_report_basic(folder)
 
